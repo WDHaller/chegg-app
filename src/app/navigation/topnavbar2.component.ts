30b25ecffb6ce9505cb1a5a24359417281d962f6
William <williamhall32798@gmail.com>
2022-01-27 00:13:59 -0500

    Started 2nd top navbar

---
+++ src/app/navigation/topnavbar2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+ selector: 'chegg-topnavbar2',
+ templateUrl: 'topnavbar2.component.html',
+ styleUrls: ['topnavbar2.component.css']
+})
+export class TopNavBar2Component {
+
+}
