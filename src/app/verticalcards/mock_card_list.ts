2cfa84888bd0dd11d7241ad2650ef0cb326ddcc2
William <williamhall32798@gmail.com>
2022-02-03 08:30:18 -0500

    Ran into issues with webp file so converted to png

--- src/app/verticalcards/mock_card_list.ts
+++ src/app/verticalcards/mock_card_list.ts
@@ -1,18 +1,18 @@
 export const mock_card_list = [
     {
-        img: "./assets/thumnail1.webp",
+        img: "./assets/thumnail1.png",
         title: "Test prep, simplified",
         text: "Check your knowledge with practice problems, quizzes, and more.",
         link: "Learn more →"
     },
     {
-        img: "./assets/thumnail2.webp",
+        img: "./assets/thumnail2.png",
         title: "Experts by your side",
         text: "Have millions of explanations at your fingertips. Plus, snap a pic to get an expert answer.",
         link: "Learn more →"
     },
     {
-        img: "./assets/thumnail3.webp",
+        img: "./assets/thumnail3.png",
         title: "Professional proofreading",
         text: "Recieve detaild comments from real writing experts on your assignment's thesis, style, and more—within 48 hours.",
         link: "Learn more →"
