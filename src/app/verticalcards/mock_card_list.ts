eb8d5141cb7a86a9279cc7ff936c74fc474778d1
William <williamhall32798@gmail.com>
2022-02-08 08:35:24 -0500

    created next row of vertical cards and added image

--- src/app/verticalcards/mock_card_list.ts
+++ src/app/verticalcards/mock_card_list.ts
@@ -16,5 +16,29 @@ export const mock_card_list = [
         title: "Professional proofreading",
         text: "Recieve detaild comments from real writing experts on your assignment's thesis, style, and more—within 48 hours.",
         link: "Learn more →"
+    },
+    {
+        img: "./assets/thumnail4.png",
+        title: "Homework help",
+        text: "Study with 55+ million step-by-step explanations, Expert Q&As & math support.",
+        link: "Learn more →"
+    },
+    {
+        img: "./assets/thumnail5.png",
+        title: "Exam prep & practice",
+        text: "Figure out what you don't know & get ready for test day with practice exams.",
+        link: "Learn more →"
+    },
+    {
+        img: "./assets/thumnail6.png",
+        title: "Understand a topic",
+        text: "Simplify the toughest concepts with digestible topic breakdowns & videos.",
+        link: "Learn more →"
+    },
+    {
+        img: "./assets/thumnail7.png",
+        title: "Writing & citations",
+        text: "Strengthen your writing with plagiarism checks, expert proofreading & instant citations.",
+        link: "Learn more →"
     }
 ];
