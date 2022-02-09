58a3b690fa6738b57f539ac5692c01e1087465a7
William <williamhall32798@gmail.com>
2022-02-09 17:12:54 -0500

    Fixing Images

--- src/app/verticalcards/cardtype1.component.ts
+++ src/app/verticalcards/cardtype1.component.ts
@@ -6,13 +6,12 @@ import { Component, Input } from "@angular/core";
     styleUrls: ['cardtype1.component.css']
 })
 export class CardType1Component {
-    @Input() img: string;
+    @Input() img!: string;
     @Input() title: string;
     @Input() text: string;
     @Input() link: string;
 
     constructor() {
-        this.img = "";
         this.title = "MISSING";
         this.text = "MISSING";
         this.link = "MISSING LINK";
--- src/app/verticalcards/mock_card_list.ts
+++ src/app/verticalcards/mock_card_list.ts
@@ -1,42 +1,42 @@
 export const mock_card_list = [
     {
-        img: "./assets/thumnail1.png",
+        img: "/assets/thumnail1.png",
         title: "Test prep, simplified",
         text: "Check your knowledge with practice problems, quizzes, and more.",
         link: "Learn more →"
     },
     {
-        img: "./assets/thumnail2.png",
+        img: "/assets/thumnail2.png",
         title: "Experts by your side",
         text: "Have millions of explanations at your fingertips. Plus, snap a pic to get an expert answer.",
         link: "Learn more →"
     },
     {
-        img: "./assets/thumnail3.png",
+        img: "/assets/thumnail3.png",
         title: "Professional proofreading",
         text: "Recieve detaild comments from real writing experts on your assignment's thesis, style, and more—within 48 hours.",
         link: "Learn more →"
     },
     {
-        img: "./assets/thumnail4.png",
+        img: "/assets/thumnail4.png",
         title: "Homework help",
         text: "Study with 55+ million step-by-step explanations, Expert Q&As & math support.",
         link: "Learn more →"
     },
     {
-        img: "./assets/thumnail5.png",
+        img: "/assets/thumnail5.png",
         title: "Exam prep & practice",
         text: "Figure out what you don't know & get ready for test day with practice exams.",
         link: "Learn more →"
     },
     {
-        img: "./assets/thumnail6.png",
+        img: "/assets/thumnail6.png",
         title: "Understand a topic",
         text: "Simplify the toughest concepts with digestible topic breakdowns & videos.",
         link: "Learn more →"
     },
     {
-        img: "./assets/thumnail7.png",
+        img: "/assets/thumnail7.png",
         title: "Writing & citations",
         text: "Strengthen your writing with plagiarism checks, expert proofreading & instant citations.",
         link: "Learn more →"
