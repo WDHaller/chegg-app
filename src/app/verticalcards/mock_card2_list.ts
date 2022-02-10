e9be2e2a3d008ee81d38be069fb59b865c288bca
William <williamhall32798@gmail.com>
2022-02-10 07:42:37 -0500

    Splitting up the card lists

---
+++ src/app/verticalcards/mock_card2_list.ts
@@ -0,0 +1,26 @@
+export const mock_card2_list = [
+    {
+        img: "/assets/thumnail4.png",
+        title: "Homework help",
+        text: "Study with 55+ million step-by-step explanations, Expert Q&As & math support.",
+        link: "Learn more →"
+    },
+    {
+        img: "/assets/thumnail5.png",
+        title: "Exam prep & practice",
+        text: "Figure out what you don't know & get ready for test day with practice exams.",
+        link: "Learn more →"
+    },
+    {
+        img: "/assets/thumnail6.png",
+        title: "Understand a topic",
+        text: "Simplify the toughest concepts with digestible topic breakdowns & videos.",
+        link: "Learn more →"
+    },
+    {
+        img: "/assets/thumnail7.png",
+        title: "Writing & citations",
+        text: "Strengthen your writing with plagiarism checks, expert proofreading & instant citations.",
+        link: "Learn more →"
+    }
+]
