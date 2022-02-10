6c12412ad1712bdc7c6931e19696bbe4c51477f3
William <williamhall32798@gmail.com>
2022-02-10 08:03:44 -0500

    Finished second row of cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CardItemModel } from './verticalcards/card-item.model';
 import { CardType1Component } from './verticalcards/cardtype1.component';
 import { mock_card_list } from './verticalcards/mock_card_list';
+import { mock_card2_list } from './verticalcards/mock_card2_list';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,19 @@ import { mock_card_list } from './verticalcards/mock_card_list';
 export class AppComponent {
   title = 'chegg';
   cards: CardItemModel [] = [];
+  card2s: CardItemModel [] = [];
 
   constructor() {
     for (var card of mock_card_list) {
       console.log(card)
       this.cards.push(card);
     }
+
+    for (var card of mock_card2_list) {
+      console.log(card)
+      this.card2s.push(card);
+    }
   }
+
+
 }
