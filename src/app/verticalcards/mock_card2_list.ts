94871ee67638057553c923f1bdddf16940e1d540
William <williamhall32798@gmail.com>
2022-02-17 05:27:35 -0500

    Corrected typo

--- src/app/verticalcards/mock_card2_list.ts
+++ src/app/verticalcards/mock_card2_list.ts
@@ -1,24 +1,24 @@
 export const mock_card2_list = [
     {
-        img: "/assets/thumnail4.png",
+        img: "/assets/thumbnail4.png",
         title: "Homework help",
         text: "Study with 55+ million step-by-step explanations, Expert Q&As & math support.",
         link: "Learn more →"
     },
     {
-        img: "/assets/thumnail5.png",
+        img: "/assets/thumbnail5.png",
         title: "Exam prep & practice",
         text: "Figure out what you don't know & get ready for test day with practice exams.",
         link: "Learn more →"
     },
     {
-        img: "/assets/thumnail6.png",
+        img: "/assets/thumbnail6.png",
         title: "Understand a topic",
         text: "Simplify the toughest concepts with digestible topic breakdowns & videos.",
         link: "Learn more →"
     },
     {
-        img: "/assets/thumnail7.png",
+        img: "/assets/thumbnail7.png",
         title: "Writing & citations",
         text: "Strengthen your writing with plagiarism checks, expert proofreading & instant citations.",
         link: "Learn more →"
--- src/app/verticalcards/mock_card_list.ts
+++ src/app/verticalcards/mock_card_list.ts
@@ -1,18 +1,18 @@
 export const mock_card_list = [
     {
-        img: "/assets/thumnail1.png",
+        img: "/assets/thumbnail1.png",
         title: "Test prep, simplified",
         text: "Check your knowledge with practice problems, quizzes, and more.",
         link: "Learn more →"
     },
     {
-        img: "/assets/thumnail2.png",
+        img: "/assets/thumbnail2.png",
         title: "Experts by your side",
         text: "Have millions of explanations at your fingertips. Plus, snap a pic to get an expert answer.",
         link: "Learn more →"
     },
     {
-        img: "/assets/thumnail3.png",
+        img: "/assets/thumbnail3.png",
         title: "Professional proofreading",
         text: "Recieve detaild comments from real writing experts on your assignment's thesis, style, and more—within 48 hours.",
         link: "Learn more →"
